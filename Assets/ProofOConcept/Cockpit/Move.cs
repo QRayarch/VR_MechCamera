81505afb6c33aee2dc780725a524bc97da7f7edd
John Park (RIT Student) <jrp3024@ad.rit.edu>
2016-10-26 12:46:09 -0400

    Cockpit proof of concept

---
+++ Assets/ProofOConcept/Cockpit/Move.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vel = Vector3.zero ;
+        if(Input.GetKey(KeyCode.W)){
+            vel += Vector3.forward * 10;
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            vel += Vector3.forward * -10;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            vel += Vector3.left * 10;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            vel += Vector3.right * 10;
+        }
+
+        transform.position = transform.position + (vel * Time.deltaTime);
+    }
+}
