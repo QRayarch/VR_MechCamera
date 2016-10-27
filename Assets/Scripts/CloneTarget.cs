969c8ebac8c980935b65482571a0f2e7c3c2251c
QRayarch <afl4947@rit.edu>
2016-10-27 13:31:44 -0400

    Added the hands being controlled by the player.

---
+++ Assets/Scripts/CloneTarget.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloneTarget : MonoBehaviour {
+
+    public Transform head;
+    public Transform objectToEffect;
+    public float scaleMultiplyer = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = transform.position - head.position;
+        offset.y = 0;
+        objectToEffect.localPosition = (head.InverseTransformPoint(transform.position)) * scaleMultiplyer;
+        objectToEffect.localRotation = transform.localRotation;
+	}
+}
---
+++ Assets/Scripts/DownPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DownPoint : MonoBehaviour {
+
+    public Transform head;
+    public float downAmount = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 headPos = head.position;
+        headPos.y -= downAmount;
+        transform.position = headPos;
+	}
+}
