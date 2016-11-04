2d48ea0a0187f570ff4b2284a9b15f7a3bfcf857
DZebraman <rpark682@gmail.com>
2016-11-03 20:01:33 -0400

    added a gun and gun camera to the mech. All that needs to be changed is the fire needs to trigger on the right controller click rather than mouse click

---
+++ Assets/Scripts/Gun/GunStuff.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunStuff : MonoBehaviour {
+
+    public GameObject firePoint;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+    void FireBullet() {
+        
+        Rigidbody rb= ((GameObject)Instantiate(bullet,firePoint.transform.position,Quaternion.identity)).GetComponent<Rigidbody>();
+        rb.AddForce(firePoint.transform.forward * 10000);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0)) {
+            FireBullet();
+        }
+	}
+}
